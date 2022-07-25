0994b40d5c774e520f480916151e0910d2784f39
Juancho <Juan.Aressi@hotmail.com>
2022-07-25 12:50:46 -0300

    Avances en la paginación

--- back/database/migrations/2022_07_11_213910_usuarios.php
+++ back/database/migrations/2022_07_11_213910_usuarios.php
@@ -4,13 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Usuarios extends Migration {
+class Usuarios extends Migration
+{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up() {
+    public function up()
+    {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->bigIncrements('id')->unique();
             $table->string('nombre')->nullable();
@@ -18,7 +20,7 @@ class Usuarios extends Migration {
             $table->string('email')->unique();
             $table->string('contraseña');
             $table->string('dni')->nullable();
-            $table->string('phone')->nullable();
+            $table->string('telefono')->nullable();
             $table->string('rol');
             $table->string('estado')->nullable();
             $table->timestamps();
@@ -30,7 +32,8 @@ class Usuarios extends Migration {
      *
      * @return void
      */
-    public function down() {
+    public function down()
+    {
         Schema::dropIfExists('usuarios');
     }
 }
