dda24622e2378ba5b3073fbcd6cb736101291f24
Juancho <Juan.Aressi@hotmail.com>
2022-08-29 18:28:07 -0300

    Ajustado los estilos y llevado al costado la barra de busqueda

--- front/src/components/SideNav/SideNav.jsx
+++ front/src/components/SideNav/SideNav.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faUserDoctor, faFileMedical, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faUser, faUserDoctor, faFileMedical, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import Logo from '../Logo/Logo';
 import './style.css';
 
 function SideNav() {
     return (
         <div id='sideNav' className='d-flex flex-column bg-secondary'>
-            <Link to='/' className='mt-3' >
+            <Link to='/panel-admin' className='mt-3' >
                 <Logo style='primary' />
             </Link>
 
             <div className='nav-container d-flex flex-column justify-content-between mt-5'>
                 <div className='d-flex flex-column justify-content-start align-items-center h-100'>
-                    <Link to='/' className='nav-item d-flex justify-content-start align-items-center text-white w-100 p-2 px-4 mt-4 active'>
+                    <Link to='/panel-admin' className='nav-item d-flex justify-content-start align-items-center text-white w-100 p-2 px-4 mt-4'>
+                        <FontAwesomeIcon
+                            className='text-white me-3'
+                            icon={faBars}
+                        />
+
+                        <h5 className='text-uppercase mb-0'>Panel</h5>
+                    </Link>
+
+                    <Link to='/panel-admin/pacientes' className='nav-item d-flex justify-content-start align-items-center text-white w-100 p-2 px-4 mt-4 active'>
                         <FontAwesomeIcon
                             className='text-white me-3'
                             icon={faUser}
