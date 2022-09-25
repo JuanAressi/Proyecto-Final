a39646dba997ddd8451980c81d21d1af2aaee10a
Juancho <Juan.Aressi@hotmail.com>
2022-09-25 12:27:16 -0300

    Avances en las validaciones del frontend del formulario de login

--- front/src/components/Input/Input.jsx
+++ front/src/components/Input/Input.jsx
@@ -2,22 +2,28 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './style.css';
 
-function Input( { id, type, name, placeholder, value, onChange, icon } ) {
+function Input( { id, classes, type, name, placeholder, value, onChange, onFocus, icon } ) {
     return (
-        <div className='icon-component form-floating w-100 mx-auto position-relative box-shadow-dark'>
+        <div className='icon-component form-floating w-100 mx-auto position-relative overflow-hidden box-shadow-dark'>
             <input
                 id={id}
-                className='form-control'
+                className={'form-control ' + classes}
                 type={type}
                 name={name}
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
+                onFocus={onFocus}
             />
 
             <label htmlFor='username' className='floatingInput'>{placeholder}</label>
 
-            {icon && <FontAwesomeIcon className='text-primary' icon={icon} />}
+            {icon &&
+                <FontAwesomeIcon
+                    className={classes === 'border-danger' ? 'text-danger' : classes === 'border-success' ? 'text-success' : 'text-primary'}
+                    icon={icon}
+                />
+            }
         </div>
     )
 }
--- front/src/pages/Login/Login.jsx
+++ front/src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { faAt, faKey } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAt, faCheck, faKey } from '@fortawesome/free-solid-svg-icons';
 import $ from 'jquery';
 import './style.css';
 import Navbar from '../../components/Navbar/Navbar';
@@ -9,24 +10,10 @@ import Input from '../../components/Input/Input';
 
 function Login() {
     const [email, setEmail] = useState('');
+    const [emailInvalidMessage, setEmailInvalidMessage] = useState('');
     const [password, setPassword] = useState('');
-    const [btnDisabled, setBtnDisabled] = useState(true);
-
-    // Use effect.
-    useEffect(() => {
-		let isValid = true;
-
-        if (email === '') { isValid = false; }
-
-        if (password === '') { isValid = false; }
-
-        if (isValid === true) {
-            setBtnDisabled(false);
-        } else {
-            setBtnDisabled(true);
-        }
-    }, [email, password]);
-
+    const [passwordInvalidMessage, setPasswordInvalidMessage] = useState('');
+    const [loginIn, setLoginIn] = useState(false);
 
     // Email change.
     const handleEmailChange = (event) => {
@@ -42,10 +29,8 @@ function Login() {
 
     // Submit Login form.
     const handleSubmitForm = (event) => {
+        // Prevent default form submit.
         event.preventDefault();
-
-        // Disable button.
-        setBtnDisabled(true);
 
         // Send request.
         $.ajax({
@@ -57,29 +42,37 @@ function Login() {
                 'password': password
             },
 			success: function (response) {
-                debugger
                 if (response.success) {
-                    // Switch user role.
+                    // Set loginIn to true.
+                    setLoginIn(true);
 
-                    switch (response.role) {
-                        case 'admin':
-                            window.location.href = '/panel-admin';
-                            break;
+                    // Wait 3 seconds to redirect.
+                    setTimeout(() => {
+                        // Switch user role.
+                        switch (response.role) {
+                            case 'admin':
+                                window.location.href = '/panel-admin';
+                                break;
 
-                        case 'administrador':
-                            window.location.href = '/panel-administrador';
-                            break;
+                            case 'administrador':
+                                window.location.href = '/panel-administrador';
+                                break;
 
-                        case 'medico':
-                            window.location.href = '/panel-medico';
-                            break;
-                        
-                        case 'paciente':
-                            window.location.href = '/';
-                            break;
-                    }
+                            case 'medico':
+                                window.location.href = '/panel-medico';
+                                break;
+                            
+                            case 'paciente':
+                                window.location.href = '/';
+                                break;
+                        }
+                    }, 3000);
                 } else {
-                    // TODO: Show error messages.
+                    if (response.field === 'email') {
+                        setEmailInvalidMessage(response.message);
+                    } else if (response.field === 'password') {
+                        setPasswordInvalidMessage(response.message);
+                    }
                 }
 			},
 			error: function (error) {
@@ -92,35 +85,68 @@ function Login() {
     return (
         <div id='loginPage'>
 			<Navbar />
-			
+
             <div className='blue-overlay pt-5'>
                 <div className='container p-0 pt-5'>
                     <div className="d-flex justify-content-center align-items-center">
                         <form id='loginForm' className='rounded box-shadow-dark p-5'>
                             <h1 className='text-center mb-3 text-darker text-shadow-dark'>¡Bienvenido!</h1>
-                            <h6 className='text-center mb-4 text-darker text-shadow-dark'>Ingresa a tu cuenta</h6>
+
+                            {loginIn &&
+                                <div className='text-center'>
+                                    <FontAwesomeIcon
+                                        className='text-success fa-2x mb-1'
+                                        icon={faCheck}
+                                    />
+                                    <h6 className='text-success text-shadow-dark fw-bold mb-3'>Iniciando sesión...</h6>
+                                </div>
+                            }
+                            
+                            {!loginIn &&
+                                <h6 className='text-center mb-4 text-darker text-shadow-dark'>Ingresa a tu cuenta</h6>
+                            }
 
                             {/* Email */}
-                            <Input
-                                id='email'
-                                type='email'
-                                name='email'
-                                placeholder='Correo electronico'
-                                value={email}
-                                onChange={handleEmailChange}
-                                icon={faAt}
-                            />
+                            <div className='mb-3'>
+                                <Input
+                                    id='email'
+                                    classes={emailInvalidMessage !== '' ? 'border-danger' : loginIn ? 'border-success' : ''}
+                                    type='email'
+                                    name='email'
+                                    placeholder='Correo electronico'
+                                    value={email}
+                                    onChange={handleEmailChange}
+                                    onFocus={() => setEmailInvalidMessage('')}
+                                    icon={faAt}
+                                />
+
+                                {emailInvalidMessage !== '' && 
+                                    <div className='invalid-feedback d-block fw-bold ps-1'>
+                                        {emailInvalidMessage}
+                                    </div>
+                                }
+                            </div>
 
                             {/* Password */}
-                            <Input
-                                id='password'
-                                type='password'
-                                name='password'
-                                placeholder='Contraseña'
-                                value={password}
-                                onChange={handlePasswordChange}
-                                icon={faKey}
-                            />
+                            <div className='mb-3' >
+                                <Input
+                                    id='password'
+                                    classes={passwordInvalidMessage !== '' ? 'border-danger' : loginIn ? 'border-success' : ''}
+                                    type='password'
+                                    name='password'
+                                    placeholder='Contraseña'
+                                    value={password}
+                                    onChange={handlePasswordChange}
+                                    onFocus={() => setPasswordInvalidMessage('')}
+                                    icon={faKey}
+                                />
+
+                                {passwordInvalidMessage !== '' &&
+                                    <div className='invalid-feedback d-block fw-bold ps-1'>
+                                        {passwordInvalidMessage}
+                                    </div>
+                                }
+                            </div>
 
                             <Link to='/recuperar-contraseña'
                                 className='text-primary text-end w-100'
@@ -134,8 +160,8 @@ function Login() {
                                     id='ingresar'
                                     className='btn bg-primary text-white box-shadow-dark text-uppercase w-50 mb-3'
                                     type='submit'
-                                    disabled={btnDisabled}
                                     onClick={handleSubmitForm}
+                                    disabled={email === '' || password === ''}
                                 >
                                     Ingresar
                                 </button>
@@ -145,7 +171,7 @@ function Login() {
                                     className='btn bg-white text-primary border border-primary box-shadow-dark text-uppercase w-50 mb-2'
                                     type='button'
                                 >
-                                    Registrate
+                                    Registrarse
                                 </Link>
                             </div>
                         </form>
