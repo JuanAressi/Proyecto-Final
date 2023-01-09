e38aef2a4eef315c1e99e0591eff8be5e4c3b3ea
Juan Manuel ARESSI <jaressi@secco.local>
2023-01-09 16:16:53 -0300

    Añadida las nuevas migraciónes de la tablas turnos_fechas y turnos_horarios

---
+++ back/database/migrations/2023_01_09_190047_turnos-horarios.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TurnosHorarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('turnos_horarios', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_turnos_fechas');
+            $table->foreign('id_turnos_fechas')->references('id')->on('turnos_fecha');
+            $table->string('horario', 5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('turnos_fecha');
+    }
+}
