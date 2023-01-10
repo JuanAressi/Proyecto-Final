fc76b376d4c4563d60d54b296f887cf032db7558
Juan Manuel ARESSI <jaressi@secco.local>
2023-01-10 14:34:56 -0300

    Realizado cambios de estilo en el banner de la pagina principal, en los botones de los turnos y en el calendario de este

--- front/src/components/Banner/Banner.jsx
+++ front/src/components/Banner/Banner.jsx
@@ -5,12 +5,12 @@ import './style.css';
 function Banner() {
     return (
         <section id='banner'>
-            <div className='banner-overlay'>
+            <div className='banner-overlay p-8'>
                 <div className='container'>
-                    <div className='row'>
-                        <div className='col-md-6 col-sm-12'>
+                    <div className='row h-100'>
+                        <div className='col-md-6 col-sm-12 my-auto'>
                             <div className='d-flex flex-column justify-content-center align-items-start'>
-                                <h1 className='text-white display-2 ms-3'>Solicitar un turno nunca fue tan facil</h1>
+                                <h1 className='text-white display-2 ms-3 mb-0'>Solicitar un turno nunca fue tan fácil</h1>
                                 <h2 className='text-white display-6 mt-4 ms-3'>¡Y al instante!</h2>
 
                                 <div className='text-center w-100'>
